a0d49198d84138a4a6d9507cff3a856d3008b7ef
mt-aya <mmht31uv.u@gmail.com>
2021-01-01 03:05:57 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :staff_users
-  devise_for :administrators
+  devise_for :staff_users, constrollers: {
+    registrations: 'staff_users/registrations',
+    sessions: 'staff_users/sessions'
+  }
+  devise_for :administrators, constrollers: {
+    resistrations: 'administrations/registrations',
+    sessions: 'administrators/sessions'
+  }
   root to: 'shifts#index'
 end
