f9be4ffa267b08d8dbd106fcf0405eba7a324a04
mt-aya <mmht31uv.u@gmail.com>
2021-01-01 23:53:05 +0900

    ownerモデルに対応するコントローラー作成とルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :owners
   devise_for :staff_users, constrollers: {
     registrations: 'staff_users/registrations',
     sessions: 'staff_users/sessions'
   }
-  # devise_for :administrators, constrollers: {
-  #   resistrations: 'administrations/registrations',
-  #   sessions: 'administrators/sessions'
-  # }
+  devise_for :owners, constrollers: {
+    resistrations: 'owners/registrations',
+    sessions: 'owners/sessions'
+  }
   root to: 'shifts#index'
 end
