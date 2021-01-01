7e9446144c77477009fdf6e9ef372255c83d47e0
mt-aya <mmht31uv.u@gmail.com>
2021-01-01 23:46:31 +0900

    owners テーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :owners
   devise_for :staff_users, constrollers: {
     registrations: 'staff_users/registrations',
     sessions: 'staff_users/sessions'
   }
-  devise_for :administrators, constrollers: {
-    resistrations: 'administrations/registrations',
-    sessions: 'administrators/sessions'
-  }
+  # devise_for :administrators, constrollers: {
+  #   resistrations: 'administrations/registrations',
+  #   sessions: 'administrators/sessions'
+  # }
   root to: 'shifts#index'
 end
