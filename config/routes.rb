6713387ded66f9e956571170068ea143268ce8d4
mt-aya <mmht31uv.u@gmail.com>
2021-01-02 18:51:57 +0900

    ルーティングのスペルミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :staff_users, constrollers: {
+  devise_for :staff_users, controllers: {
     registrations: 'staff_users/registrations',
     sessions: 'staff_users/sessions'
   }
-  devise_for :owners, constrollers: {
-    resistrations: 'owners/registrations',
+  devise_for :owners, controllers: {
+    registrations: 'owners/registrations',
     sessions: 'owners/sessions'
   }
   root to: 'shifts#index'
