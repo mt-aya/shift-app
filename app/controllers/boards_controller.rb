eaa7da4ec144aeecb6072c5670d67dcbac6facfd
mt-aya <mmht31uv.u@gmail.com>
2021-01-04 08:46:05 +0900

    シフトボード一覧ページ作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,10 @@
 class BoardsController < ApplicationController
 
+  def index
+    owner = Owner.find(current_owner.id)
+    @boards = owner.boards.order('created_at DESC')
+  end
+
   def new
     @board = Board.new
   end
