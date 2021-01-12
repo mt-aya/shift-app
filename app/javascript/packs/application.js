bd659fc989f7bbdf64bb7e7aefb702a78f092603
mt-aya <mmht31uv.u@gmail.com>
2021-01-12 16:43:10 +0900

    表示切り替え機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../modal_board")
 require("../board_list")
+require("../switch")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
