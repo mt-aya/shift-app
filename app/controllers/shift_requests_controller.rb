3485974f8f73d34adaf8b2519ec109801064a807
mt-aya <mmht31uv.u@gmail.com>
2021-01-23 20:09:34 +0900

    確定シフト・希望シフト一覧ページ作成

--- app/controllers/shift_requests_controller.rb
+++ app/controllers/shift_requests_controller.rb
@@ -1,5 +1,9 @@
 class ShiftRequestsController < ApplicationController
 
   def index
+    if staff_user_signed_in?
+      @shift_requests = current_staff_user.shift_requests
+      @shifts = current_staff_user.shifts
+    end
   end
 end
