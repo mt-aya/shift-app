d25d2911795d5702108d469d7a16fe1a8570e123
mt-aya <mmht31uv.u@gmail.com>
2021-01-27 16:31:30 +0900

    エラーメッセージの日本語化

--- spec/models/staff_user_spec.rb
+++ spec/models/staff_user_spec.rb
@@ -43,43 +43,43 @@ RSpec.describe StaffUser, type: :model do
       it 'emailが空だと登録できない' do
         @staff_user.email = ''
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include("Email can't be blank")
+        expect(@staff_user.errors.full_messages).to include("Eメールを入力してください")
       end
 
       it 'passwordが空だと登録できない' do
         @staff_user.password = ''
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include("Password can't be blank")
+        expect(@staff_user.errors.full_messages).to include("パスワードを入力してください")
       end
 
       it 'passwordが存在していてもpassword_confirmationが空だと登録できない' do
         @staff_user.password_confirmation = ''
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@staff_user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
 
       it 'id_nameが空だと登録できない' do
         @staff_user.id_name = ''
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include("Id name can't be blank")
+        expect(@staff_user.errors.full_messages).to include("ユーザーIDを入力してください")
       end
 
       it 'last_nameが空だと登録できない' do
         @staff_user.last_name = ''
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include("Last name can't be blank")
+        expect(@staff_user.errors.full_messages).to include("お名前（姓）を入力してください")
       end
 
       it 'first_nameが空だと保存できない' do
         @staff_user.first_name = ''
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include("First name can't be blank")
+        expect(@staff_user.errors.full_messages).to include("お名前（名）を入力してください")
       end
 
       it 'emailは@が含まれていないと登録できない' do
         @staff_user.email = 'email'
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Email is invalid')
+        expect(@staff_user.errors.full_messages).to include('Eメールは不正な値です')
       end
 
       it 'passwordは数字だけでは登録できない' do
@@ -87,7 +87,7 @@ RSpec.describe StaffUser, type: :model do
         @staff_user.password = staff_user_password
         @staff_user.password_confirmation = staff_user_password
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Password is invalid')
+        expect(@staff_user.errors.full_messages).to include('パスワードは不正な値です')
       end
 
       it 'passwordは英字だけでは登録できない' do
@@ -95,7 +95,7 @@ RSpec.describe StaffUser, type: :model do
         @staff_user.password = staff_user_password
         @staff_user.password_confirmation = staff_user_password
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Password is invalid')
+        expect(@staff_user.errors.full_messages).to include('パスワードは不正な値です')
       end
 
       it 'passwordは英・数字混合でも5文字以下だと登録できない' do
@@ -103,7 +103,7 @@ RSpec.describe StaffUser, type: :model do
         @staff_user.password = staff_user_password
         @staff_user.password_confirmation = staff_user_password
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+        expect(@staff_user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
       end
 
       it '重複したemailが既に存在する場合は登録できない' do
@@ -111,37 +111,37 @@ RSpec.describe StaffUser, type: :model do
         another_staff_user = FactoryBot.build(:staff_user)
         another_staff_user.email = @staff_user.email
         another_staff_user.valid?
-        expect(another_staff_user.errors.full_messages).to include('Email has already been taken')
+        expect(another_staff_user.errors.full_messages).to include('Eメールはすでに存在します')
       end
 
       it 'id_nameは6~16文字でも全角だと登録できない' do
         @staff_user.id_name = 'あいうえおア'
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Id name is invalid')
+        expect(@staff_user.errors.full_messages).to include('ユーザーIDは不正な値です')
       end
 
       it 'id_nameは5文字以下だと登録できない' do
         @staff_user.id_name = 'abcde'
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Id name is invalid')
+        expect(@staff_user.errors.full_messages).to include('ユーザーIDは不正な値です')
       end
 
       it 'id_nameは17文字以上だと登録できない' do
         @staff_user.id_name = '1234567890abcdefg'
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Id name is invalid')
+        expect(@staff_user.errors.full_messages).to include('ユーザーIDは不正な値です')
       end
 
       it 'last_nameは半角だと登録できない' do
         @staff_user.last_name = 'hankaku'
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('Last name is invalid')
+        expect(@staff_user.errors.full_messages).to include('お名前（姓）は全角文字で入力してください')
       end
 
       it 'first_nameは半角だと登録できない' do
         @staff_user.first_name = 'hankaku'
         @staff_user.valid?
-        expect(@staff_user.errors.full_messages).to include('First name is invalid')
+        expect(@staff_user.errors.full_messages).to include('お名前（名）は全角文字で入力してください')
       end
     end
   end
