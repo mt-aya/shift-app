466efc1456e68f2cc33202602376363ceaf32cd6
mt-aya <mmht31uv.u@gmail.com>
2021-02-08 23:00:50 +0900

    shift_requestsテーブルのカラム名スペルミス修正

--- db/migrate/20210122093402_create_shift_requests.rb
+++ db/migrate/20210122093402_create_shift_requests.rb
@@ -3,7 +3,7 @@ class CreateShiftRequests < ActiveRecord::Migration[6.0]
     create_table :shift_requests do |t|
       t.datetime   :start_time, null: false
       t.datetime   :end_time,   null: false
-      t.boolean    :sibmited
+      t.boolean    :submitted
       t.references :staff_user, foreign_key: true
       t.references :board,      foreign_key: true
       t.timestamps
