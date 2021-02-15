6e269fb382ad870999084d82bdc6fd9403e5993f
mt-aya <mmht31uv.u@gmail.com>
2021-02-16 02:25:09 +0900

    capybaraの設定修正（条件式ミス修正2）

--- spec/helpers/capybara.rb
+++ spec/helpers/capybara.rb
@@ -2,7 +2,7 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    if ENV.("SELENIUM_DRIVER_URL").present?
+    if ENV["SELENIUM_DRIVER_URL"].present?
       driven_by :selenium, using: :headless_chrome, options: {
         browser: :remote,
         url: ENV.fetch("SELENIUM_DRIVER_URL"),
