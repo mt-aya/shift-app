50b5218b05ca3148a1c376acc479de14d732b8f5
mt-aya <mmht31uv.u@gmail.com>
2021-02-16 02:17:23 +0900

    localでのテストとCircleCIでのテストの条件分け

--- spec/helpers/capybara.rb
+++ spec/helpers/capybara.rb
@@ -2,12 +2,16 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome, options: {
-      browser: :remote,
-      url: ENV.fetch("SELENIUM_DRIVER_URL"),
-      desired_capabilities: :chrome
-    }
-    Capybara.server_host = 'web'
-    Capybara.app_host='http://web'
+    if ENV.fetch("SELENIUM_DRIVER_URL").present?
+      driven_by :selenium, using: :headless_chrome, options: {
+        browser: :remote,
+        url: ENV.fetch("SELENIUM_DRIVER_URL"),
+        desired_capabilities: :chrome
+      }
+      Capybara.server_host = 'web'
+      Capybara.app_host='http://web'
+    else
+      :selenium_chrome_headless
+    end
   end
 end
