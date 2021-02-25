8d3a84419627ade49b53c80de9fdc8880ecaba8e
mt-aya <mmht31uv.u@gmail.com>
2021-02-25 18:43:46 +0900

    シフト共有機能のうちの期間指定によるシフト検索機能実装

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,7 +1,9 @@
 class ShiftsController < ApplicationController
   before_action :except_owner
-  before_action :correct_owner, only: [:index]
+  before_action :correct_owner, only: [:index, :monthly, :weekly, :calendar, :create, :search, :share]
+  before_action :header_set, only: [:index_set, :search, :share]
   before_action :index_set, only: [:index, :monthly, :weekly, :calendar, :create]
+
 
   def index
   end
@@ -42,15 +44,26 @@ class ShiftsController < ApplicationController
   end
 
   def search
+    @search_params = search_params
+    if @search_params[:start_term].present? && search_params[:end_term].present?
+      @shifts = Shift.search(@search_params).includes(:board, :staff_user).order(start_time: 'ASC')
+      session[:search] = @shifts.ids
+    else
+      render :search
+    end
   end
 
-  def submit
+  def share
   end
 
   private
 
   def shift_params
     params.require(:shift).permit(:start_time, :end_time).merge(staff_user_id: params[:staff_user_id], board_id: params[:board_id])
+  end
+
+  def search_params
+    params.fetch(:search, {}).permit(:start_term, :end_term).merge(board_id: params[:board_id])
   end
 
   def except_owner
@@ -63,14 +76,17 @@ class ShiftsController < ApplicationController
   end
 
   def index_set
+    @shifts = @board.shifts
+    @shift = Shift.new
+    @staffs = @board.staff_users
+  end
+
+  def header_set
     @board = Board.find(params[:board_id])
     gon.board_id = @board.id
     gon.board_name = @board.name
     @board_staff = BoardStaff.new
     @board_staffs = @board.staff_users
     gon.board_staffs_id = @board_staffs.map { |s| s[:id] }
-    @shifts = @board.shifts
-    @shift = Shift.new
-    @staffs = @board.staff_users
   end
 end
